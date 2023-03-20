e33df591fcd5636c930422f7f2ec8dda614c0f53
allanstar-byte <allanmutisya263@gmail.com>
2023-03-20 22:39:53 +0300

    Creating end points

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+
+def drink_list(request):
+    # get all the drinks
+    # serialize them
+    # return json
