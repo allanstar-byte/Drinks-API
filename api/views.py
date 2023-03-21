74a7dcb2142c6fffbde5f6a165243b44a9c9858e
allanstar-byte <allanmutisya263@gmail.com>
2023-03-21 12:34:43 +0300

    Getting the drinks API

--- api/views.py
+++ api/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
 
 def drink_list(request):
     # get all the drinks
+    drinks = Drink.objects.all()
     # serialize them
+    serializer = DrinkSerializer(drinks, many=True)
     # return json
+    return JsonResponse({"drinks": serializer.data}, safe=False)
